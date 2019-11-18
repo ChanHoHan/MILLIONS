1078089550a0acdf4820952703b4f17b34533603
zeus <wpdn79@likelion.org>
2019-11-18 14:08:42 +0900

    test

--- MILLIONS/gettimer.js
+++ MILLIONS/gettimer.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 import ENV from "./environment";
 
-<<<<<<< HEAD
 axios.defaults.baseURL = ENV.BACKEND_HOST;
-=======
-axios.defaults.baseURL = ENV.LOCAL_IP;
->>>>>>> a4b881ec83dbd0a2eb9c7e6e4ad3bfce339ad891
 
 export default {
   // 모든 글 불러오기
